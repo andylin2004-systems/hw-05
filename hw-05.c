4e0d3ba2042c9be70500a054f5f28983a8803d19
Andy Lin <60021592+andylin2004@users.noreply.github.com>
2021-09-29 12:22:15 -0400

    did steps 3 and 4 to hw-05

--- hw-05.c
+++ hw-05.c
@@ -10,4 +10,10 @@ int main(){
     printf("char mem address decimal: %lu\n", &character);
     printf("int mem address decimal: %lu\n", &integer);
     printf("long mem address decimal: %lu\n", &longNum);
+    char *characterPointer = &character;
+    int *integerPointer = &integer;
+    long *longPointer = &longNum;
+    printf("char mem pointer address: %p\n", characterPointer);
+    printf("int mem pointer address: %p\n", integerPointer);
+    printf("long mem pointer address: %p\n", longPointer);
 }
