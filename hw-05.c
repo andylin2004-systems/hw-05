724ad91b8fe4d954fc02da3decb0d444853fd94f
Andy Lin <60021592+andylin2004@users.noreply.github.com>
2021-09-29 12:17:48 -0400

    inital commit of hw-05 (steps 1 and 2)

---
+++ hw-05.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+    char character = "h";
+    int integer = 1;
+    long longNum = 1;
+    printf("char mem address hex: %p\n", character);
+    printf("int mem address hex: %p\n", integer);
+    printf("long mem address hex: %p\n", longNum);
+    printf("char mem address decimal: %lu\n", character);
+    printf("int mem address decimal: %lu\n", integer);
+    printf("long mem address decimal: %lu\n", longNum);
+}
